f7a6c1bb1dc31851e4e90376a6b57a787dc357cc
mercmobily <tonymobily@gmail.com>
2026-09-21 13:47:20 +0800

    fix: discover JSKIT and Vuetify dependencies before page load

--- vite.config.mjs
+++ vite.config.mjs
@@ -55,6 +55,7 @@ export default defineConfig({
     include: ["tests/client/**/*.vitest.js"]
   },
   optimizeDeps: {
+    include: ["vuetify/components/**", "vuetify/directives/**"],
     entries: [
       "index.html",
       "src/**/*.{js,ts,vue}"
